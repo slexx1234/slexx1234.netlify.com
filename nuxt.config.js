891aa8437163f33325be8b42305380cb7da5a248
Alex <slexx1234@gmail.com>
2018-04-22 17:40:23 +0300

    Поправил тайтлы, пытаюсь сделать редирект

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,7 +5,7 @@ module.exports = {
     ** Headers of the page
     */
     head: {
-        title: 'resume',
+        title: 'slexx1234.netlify.com',
         meta: [
             { charset: 'utf-8' },
             { name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no' },
